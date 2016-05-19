040fac73b85ccb7c9cfff057f1c0bdc15264c1c7
Michael Kennedy <Michael Kennedy>
2016-05-19 14:16:03 +0100

    code from applied nosql session on thrusday

---
+++ applied_nosql_dotnet/AppliedNoSqlSDD/Db.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace AppliedNoSqlSDD
+{
+	class Db
+	{
+		public IMongoDatabase db;
+
+		public Db()
+		{
+			var client = new MongoClient();
+			this.db = client.GetDatabase("sdd_blog");
+		}
+
+		public IMongoCollection<Post> Posts
+		{
+			get { return db.GetCollection<Post>("Post"); }
+		}
+
+		public IMongoCollection<User> Users
+		{
+			get { return db.GetCollection<User>("User"); }
+		}
+	}
+
+	[BsonIgnoreExtraElements]
+	class Post// : ISupportInitialize
+	{
+		public ObjectId Id { get; set; }
+		public string Title { get; set; }
+		public DateTime Published { get; set; }
+		public string Url { get; set; }
+		public List<Comment> Comments { get; set; }
+
+		public string PublishedText { get; set; }
+
+		public Post()
+		{
+			this.Published = DateTime.Now;
+			this.Comments = new List<Comment>();
+		}
+	}
+
+	struct Comment
+	{
+		public string Name { get; set; }
+		public DateTime Created { get; set; }
+		public string CommentText { get; set; }
+
+		//public Comment()
+		//{
+		//	this.Created = DateTime.Now;
+		//}
+	}
+
+	class User
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+
+		public User()
+		{
+			Id = Guid.NewGuid().ToString("N");
+		}
+	}
+}
---
+++ applied_nosql_dotnet/AppliedNoSqlSDD/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace AppliedNoSqlSDD
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			InsertSomeData();
+		}
+
+		private static void InsertSomeData()
+		{
+			//Post p = new Post();
+			//p.Title = "My first mongoDB example";
+			//p.Url = "/go/mongo";
+			Post p = new Post();
+			p.Title = "Follow up on mongo";
+			p.Url = "/go/mongo/2";
+
+			var db = new Db();
+			//db.Posts.InsertOne(p);
+
+			var firstPost = db.Posts
+				.AsQueryable()
+				.First(post => post.Url == "/go/mongo/2");
+
+			Console.WriteLine(firstPost.Title);
+			// Console.WriteLine("Has {0} views", firstPost.ViewCount);
+			//Console.WriteLine(firstPost.AddtionalData);
+
+			//Comment c = new Comment();
+			//c.CommentText = "Fantastic post, thanks";
+			//c.Name = "Jeff";
+			//firstPost.Comments.Add(c);
+
+			//c = new Comment();
+			//c.CommentText = "Great post, love it. Get your wordpress themes here: ...";
+			//c.Name = "Sarah";
+			//firstPost.Comments.Add(c);
+
+			//db.Posts.ReplaceOne(ps => ps.Id == firstPost.Id, firstPost);
+
+
+			//foreach (var post in db.Posts.AsQueryable())
+			//{
+			//	db.Posts.ReplaceOne(ps => ps.Id == post.Id, post);
+			//}
+
+			var jeffPosts =
+				from pt in db.Posts.AsQueryable()
+				where pt.Comments.Any(c => c.Name == "Jeff")
+				select pt;
+
+			Console.WriteLine("Jeffs Posts");
+			foreach (var jp in jeffPosts)
+			{
+
+				Console.WriteLine(jp.Title);
+			}
+
+			var user = new User();
+			user.Name = "root";
+			db.Users.InsertOne(user);
+		}
+	}
+}
